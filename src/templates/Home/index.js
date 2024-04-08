0074b6f4b87f7e6da1b77414f42ebf4a93c9cb05
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-09 00:40:55 +0330

    feat: Incorporate Profile component into Home

--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -1,10 +1,17 @@
 import { El } from "@/utils";
-import { Profile } from "./Profile";
+import { Profile } from "@/components/home/profile";
+import { getUser } from "@/library/api/users";
 
 export const Home = () => {
+  const profileElement = El({
+    element: "div",
+  });
+  getUser(1).then((res) => {
+    profileElement.append(Profile(res));
+  });
   return El({
     element: "div",
     className: "w-428 flex flex-col",
-    children: [Profile()],
+    children: [profileElement],
   });
 };
