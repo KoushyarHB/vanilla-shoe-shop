3d0b2e9a82354d50696a7fe3bdd11a8d5b30be84
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-09 16:34:40 +0330

    feat: Build Brands component

---
+++ src/components/home/brands/index.js
@@ -0,0 +1,35 @@
+import { El } from "@/utils";
+import { brandNames } from "@/localization/constants/brandNames";
+
+export const Brands = () => {
+  const brands = [...brandNames, "More"];
+  return El({
+    element: "div",
+    className: "grid grid-cols-4 px-3 py-2",
+    children: brands.map((brandName) => {
+      return El({
+        element: "div",
+        className: "flex flex-col items-center gap-3 mb-7",
+        children: [
+          El({
+            element: "button",
+            className:
+              "flex justify-center items-center bg-[#ECECEC] w-16 h-16 rounded-full",
+            children: [
+              El({
+                element: "img",
+                src: "./src/assets/img/brand-logos/" + brandName + ".png",
+              }),
+            ],
+          }),
+          El({
+            element: "span",
+            className:
+              "text-sm font-semibold max-w-64 overflow-hidden overflow-ellipsis text-center whitespace-nowrap",
+            innerText: brandName !== "More" ? brandName : "More ..",
+          }),
+        ],
+      });
+    }),
+  });
+};
