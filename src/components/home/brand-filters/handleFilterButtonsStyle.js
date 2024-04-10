5e13d097b48610a2a21d0f7ec981a0f165c8eda0
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-10 22:23:31 +0330

    fix: Remove extra code from function

--- src/components/home/brand-filters/handleFilterButtonsStyle.js
+++ src/components/home/brand-filters/handleFilterButtonsStyle.js
@@ -1,7 +1,6 @@
 import { filterButtons } from "./createFilterButtons";
 
 export const handleFilterButtonsStyle = (e) => {
-  console.log(e.target.innerText);
   for (let filterButton of filterButtons) {
     if (filterButton.innerText === e.target.innerText) {
       filterButton.classList.add("selected");
