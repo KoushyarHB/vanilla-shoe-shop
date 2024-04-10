47ceaf61c85ded0b5b36170f86a00287f4a41c5d
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-10 16:36:38 +0330

    fix: Modify renderProduct with Link

--- src/components/home/products/renderProduct.js
+++ src/components/home/products/renderProduct.js
@@ -1,33 +1,39 @@
 import { El } from "@/utils";
+import { Link } from "@/components/link";
 
 export const renderProduct = (obj) => {
-  console.log(obj.title);
   return El({
     element: "div",
-    className: "flex flex-col mb-6",
+    id: obj.id,
     children: [
-      El({
-        element: "div",
-        className:
-          "w-182 h-182 flex justify-center items-center bg-[#EBEFF2] rounded-3xl mb-3",
+      Link({
+        href: `/products/${obj.id}`,
+        className: "flex flex-col mb-6",
         children: [
           El({
-            element: "img",
-            className: "w-142 h-142",
-            src: obj.imageURL,
+            element: "div",
+            className:
+              "w-182 h-182 flex justify-center items-center bg-[#EBEFF2] rounded-3xl mb-3",
+            children: [
+              El({
+                element: "img",
+                className: "w-142 h-142",
+                src: obj.imageURL,
+              }),
+            ],
+          }),
+          El({
+            element: "span",
+            className:
+              "text-xl font-bold mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap",
+            innerText: obj.name,
+          }),
+          El({
+            element: "span",
+            className: "text-base font-semibold",
+            innerText: `$ ${obj.price}.00`,
           }),
         ],
-      }),
-      El({
-        element: "span",
-        className:
-          "text-xl font-bold mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap",
-        innerText: obj.name,
-      }),
-      El({
-        element: "span",
-        className: "text-base font-semibold",
-        innerText: `$ ${obj.price}.00`,
       }),
     ],
   });
