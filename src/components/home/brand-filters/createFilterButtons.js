ba33be0f0dcfc628286468b32acf63b34f978c1f
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-10 16:07:10 +0330

    feat: Add class to filter buttons

--- src/components/home/brand-filters/createFilterButtons.js
+++ src/components/home/brand-filters/createFilterButtons.js
@@ -4,7 +4,7 @@ import { brandNames } from "@/localization/constants/brandNames";
 const filterButtonTexts = ["All", ...brandNames];
 export const filterButtons = filterButtonTexts.map((text) => {
   const classesString =
-    "text-base font-semibold text-[#343A40] border-2 border-[#343A40] rounded-full whitespace-nowrap px-5 py-2 mr-3";
+    "filter-button text-base font-semibold text-[#343A40] border-2 border-[#343A40] rounded-full whitespace-nowrap px-5 py-2 mr-3";
   const classes = text !== "All" ? classesString : classesString + " selected";
   return El({
     element: "button",
