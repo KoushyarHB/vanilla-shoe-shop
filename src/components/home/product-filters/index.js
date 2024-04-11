0313ffaca178362c92402893acddbdbb9f715ff1
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-11 12:23:02 +0330

    fix: Modify 'Most Popular' and 'See All'

--- src/components/home/product-filters/index.js
+++ src/components/home/product-filters/index.js
@@ -1,16 +1,19 @@
 import { El } from "@/utils";
+import { Link } from "@/components/link";
 
 export const ProductFilters = () => {
   return El({
     element: "div",
     className: "flex justify-between items-center mb-5",
     children: [
-      El({
+      Link({
+        href: `/most-popular`,
         element: "button",
         className: "text-base font-semibold",
         innerText: "Most Popular",
       }),
-      El({
+      Link({
+        href: `/most-popular`,
         element: "button",
         className: "text-base font-semibold",
         innerText: "See All",
