faecd0fec1f30e43addc3e57ad6eeafa4afd3d0a
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-11 12:24:36 +0330

    style: Add new custom style to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { addDynamicIconSelectors } from "@iconify/tailwind";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./main.js"],
   theme: {
@@ -19,7 +20,10 @@ export default {
       maxWidth: {
         64: "64px",
       },
+      maxHeight: {
+        784: "784px",
+      },
     },
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()],
 };
