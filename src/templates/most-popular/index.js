5cb76192a75ee75a7598b406d8377f47468b9c96
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-20 23:49:13 +0330

    feat: Add Header component to MostPopular template

--- src/templates/most-popular/index.js
+++ src/templates/most-popular/index.js
@@ -2,37 +2,10 @@ import { El } from "@/utils";
 import { getProductsByBrandName } from "@/library/api/users";
 import { renderProducts } from "@/components/home/products/renderProducts";
 import { BrandFilters } from "@/components/home/brand-filters";
+import { Header } from "@/layout/header";
 
 export function MostPopular() {
-  const MostPopularPageHeader = El({
-    element: "div",
-    className: "flex items-center gap-3 pb-8",
-    children: [
-      El({
-        element: "button",
-        className: "flex items-center",
-        eventListener: [
-          {
-            event: "click",
-            callback: () => {
-              window.history.back();
-            },
-          },
-        ],
-        children: [
-          El({
-            element: "span",
-            className: "icon-[ep--back] text-2xl",
-          }),
-        ],
-      }),
-      El({
-        element: "h1",
-        className: "text-xl font-semibold",
-        innerText: "Most Popular",
-      }),
-    ],
-  });
+  const MostPopularPageHeader = Header("Most Popular");
 
   const MostPopularPageBody = El({
     element: "div",
@@ -40,16 +13,17 @@ export function MostPopular() {
     className: "grid grid-cols-2 gap-4 overflow-y-scroll scrollbar-hide mb-10",
   });
   getProductsByBrandName("ALL").then((res) => {
-    renderMostPopularPage(MostPopularPageBody, res);
+    renderProducts(MostPopularPageBody, res);
   });
 
   const page = El({
     element: "div",
-    children: [MostPopularPageHeader, BrandFilters(), MostPopularPageBody],
+    children: [
+      MostPopularPageHeader,
+      El({ element: "div", className: "mt-16", children: [BrandFilters()] }),
+      MostPopularPageBody,
+    ],
   });
-  return page;
-}
 
-function renderMostPopularPage(elem, res) {
-  renderProducts(elem, res);
+  return page;
 }
