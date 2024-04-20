dc34a2f97a470fc2ded78d0b3252050a0d8fac2c
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-20 23:36:23 +0330

    feat: Add app texts to constants folder

---
+++ src/localization/constants/texts.js
@@ -0,0 +1,28 @@
+export const welcomeMsg = {
+  1: "Welcome to 👋",
+  2: "The best sneakers & shoes e-commerse app of the century for your fashion needs!",
+};
+
+export const swiperMsg = {
+  1: "We provide high quality products just for you",
+  2: "Your satisfaction is our number one periority",
+  3: "Let’s fulfill your fashion needs with shoea right now!",
+};
+
+export const formMsg = {
+  1: "Login to Your Account",
+};
+
+export const loginMsg = {
+  1: "Please enter your email!",
+  2: "This email is not registered. Please sign up.",
+  3: "Please enter your password!",
+  4: "Wrong password.",
+};
+
+export const paymentMsg = {
+  1: "Order Successful!",
+  2: "You have successfully made order",
+  3: "View Order",
+  4: "View E-Receipt",
+};
