2c7d722d73ba5a268ff2ad8f673ff6ce1a8000be
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-20 23:19:24 +0330

    feat: Add handleFilterButtonAction function

--- src/components/home/brand-filters/handleFilterButtonAction.js
+++ src/components/home/brand-filters/handleFilterButtonAction.js
@@ -2,12 +2,12 @@ import { getProductsByBrandName } from "@/library/api/users";
 import { renderProducts } from "../products/renderProducts";
 
 export const handleFilterButtonAction = (e) => {
-  e.preventDefault();
+  e.stopPropagation();
   const query = e.target.innerText.toUpperCase();
   const productsDiv = document.getElementById("productsAtHome");
   productsDiv.innerHTML = "";
   getProductsByBrandName(query).then((res) => {
-    console.log(res);
+    // console.log(res);
     productsDiv.innerHTML = "";
     renderProducts(productsDiv, res);
   });
