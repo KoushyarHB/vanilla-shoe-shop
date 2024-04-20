4b7c83749ab8dd300cdb8b1709fe0b8fa83ff9c5
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-20 23:39:05 +0330

    style: Add styles to BrandPage

--- src/pages/brand/index.js
+++ src/pages/brand/index.js
@@ -5,7 +5,7 @@ const BrandPage = (data) => {
   document.title = "Brand Page";
   return El({
     element: "div",
-    className: "w-428 px-6 py-4 border",
+    className: "w-428 h-926 overflow-y-scroll scrollbar-hide px-6 py-4 border",
     children: [Brand(data)],
   });
 };
