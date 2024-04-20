03b3d893f2dbae807ae56d02ed024bea0bd74d63
Koushyar <koushyarheidaribateni@gmail.com>
2024-04-20 23:41:07 +0330

    style: Add classes to MostPopularPage

--- src/pages/most-popular/index.js
+++ src/pages/most-popular/index.js
@@ -5,7 +5,7 @@ const MostPopularPage = () => {
   document.title = "Most Popular Page";
   return El({
     element: "div",
-    className: "w-428 px-6 py-4 border",
+    className: "w-428 h-926 overflow-y-scroll scrollbar-hide px-6 py-4 border",
     children: [MostPopular()],
   });
 };
